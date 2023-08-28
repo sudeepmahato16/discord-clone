81e2b120e655ea5aa407c68e7ce70dabe8ebe1b4
sudeepmahato16 <sudeepmahato16@gmail.com>
2023-08-28 14:53:54 +0545

    Setup dark and light theme

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/ui/ModeToggle";
 
 const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 };
